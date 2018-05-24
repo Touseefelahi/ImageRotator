d876310354cea4d4cab66878b6fbdb6e4c688699
Touseef Elahi <touseefelahi@ymail.com>
2018-05-24 10:46:39 +0300

    managed memory leaks

--- ImageRotator/ViewModels/ImageViewModel.cs
+++ ImageRotator/ViewModels/ImageViewModel.cs
@@ -12,24 +12,39 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Threading;
 using ImageRotator.Models;
+using System.Runtime.InteropServices;
 
 namespace ImageRotator.Core
 {
     public class ImageViewModel : BaseViewModel
     {
         public Image<Bgr, byte> Graphics { get; set; }
-        Image<Bgr, byte> GraphicsDefault { get; set; }
-
+        
         public ICommand CommandOpenImage { get; set; }
         public ICommand CommandFlipImage { get; set; }
         public ICommand CommandSaveImage { get; set; }
         public ICommand CommandMouseUp { get; set; }
         public ICommand CommandShowDefault { get; set; }
         public ICommand CommandMouseMove { get; set; }
+        public ICommand CommandResetFirstPoint { get; set; }
         public bool IsVerticallyAligned { get; set; } = true;
         public string PathOfFile { get; set; }
+        public double ToolTipX { get; set; }
+        public double ToolTipY { get; set; }
+
+        string toolTipText;
+        public string ToolTipText
+        {
+            get => toolTipText;
+            set
+            {
+                toolTipText = IsPointA ? "Select First Point" : "Select Second Point" + Environment.NewLine + value;
+            }
+        }
         public Point PointA { get; set; }
         public Point PointB { get; set; }
+
+
         public Point MousePositionCurrent { get; set; }
 
         SemaphoreSlim signalForNextImage = new SemaphoreSlim(0);
@@ -38,6 +53,7 @@ namespace ImageRotator.Core
         public bool IsPointA { get; set; } = true;
         public string Info { get; private set; } = "Open and select image files to get started";
         public string ImageCounterDisplay { get; set; }
+        public bool IsBusy { get; private set; }
 
         int totalImages;
         int currentImageNumber;
@@ -50,16 +66,19 @@ namespace ImageRotator.Core
         double baseTriangle;
         double perpendicularTriangle;
         double hypotenuseTriangle;
-        Bgr background = new Bgr(0,0,0);
+        Bgr background = new Bgr(0, 0, 0);
+
         public ImageViewModel()
         {
             CommandOpenImage = new RelayCommand(OpenImage);
-            CommandSaveImage = new RelayCommand(SaveImage);
+            CommandSaveImage = new RelayCommand(async () => await SaveImageAsync());
             CommandFlipImage = new RelayCommand(FlipImage);
-            CommandShowDefault = new RelayCommand(ShowDefaultImage);
+            CommandShowDefault = new RelayCommand(async () => await ShowDefaultImageAsync());
             CommandMouseUp = new RelayParameterizedCommand((parameter) => MouseUp(parameter));
             CommandMouseMove = new RelayParameterizedCommand((parameter) => MouseMove(parameter));
+            CommandResetFirstPoint = new RelayCommand(() => IsPointA = true);
         }
+
 
         private void FlipImage()
         {
@@ -74,10 +93,26 @@ namespace ImageRotator.Core
             }
         }
 
-        private void ShowDefaultImage()
+        private async Task ShowDefaultImageAsync()
         {
-            Graphics = GraphicsDefault.Clone();
-            IsPointA = true;
+            if (!IsBusy)
+            {
+                IsBusy = true;
+                try
+                {
+
+                    await Task.Run(() => Graphics.Dispose());
+                    Graphics = new Image<Bgr, byte>(fileName);
+                    Info = "Default image loaded";
+                    IsPointA = true;
+                }
+                catch (Exception ex)
+                {
+                    Info = ex.Message;
+                }
+                await Task.Delay(200);
+                IsBusy = false;
+            }
         }
 
         private void MouseUp(object parameter)
@@ -96,59 +131,80 @@ namespace ImageRotator.Core
                 else PointB = point;
 
                 IsPointA = !IsPointA;
-                if (IsPointA) RotateImage();
+                if (IsPointA) RotateImageAsync();
             }
 
         }
 
-        private void RotateImage()
+        private async void RotateImageAsync()
         {
-            if (PointA != null && PointB != null)
+            if (!IsBusy)
             {
-                baseTriangle = Math.Abs(PointB.X - PointA.X);
-                perpendicularTriangle = Math.Abs(PointB.Y - PointA.Y);
-                hypotenuseTriangle = Math.Sqrt(Math.Pow(baseTriangle, 2) + Math.Pow(perpendicularTriangle, 2));
-                var angleRadian = Math.Atan(perpendicularTriangle / baseTriangle);
-                Angle = angleRadian * 180.0 / Math.PI;
+                IsBusy = true;
+                try
+                {
+                    if (PointA != null && PointB != null)
+                    {
+                        baseTriangle = Math.Abs(PointB.X - PointA.X);
+                        perpendicularTriangle = Math.Abs(PointB.Y - PointA.Y);
+                        hypotenuseTriangle = Math.Sqrt(Math.Pow(baseTriangle, 2) + Math.Pow(perpendicularTriangle, 2));
+                        var angleRadian = Math.Atan(perpendicularTriangle / baseTriangle);
+                        Angle = angleRadian * 180.0 / Math.PI;
 
-                var position = Position.Get(PointA, PointB);
-                if (IsVerticallyAligned)
-                {
-                    Angle = 90 - Angle;
-                    switch (position)
-                    {
-                        case FirstPointPosition.LeftDown:
-                        case FirstPointPosition.RightUp:
-                            Angle = (-1) * Angle;
-                            break;
+                        var position = Position.Get(PointA, PointB);
+                        if (IsVerticallyAligned)
+                        {
+                            Angle = 90 - Angle;
+                            switch (position)
+                            {
+                                case FirstPointPosition.LeftDown:
+                                case FirstPointPosition.RightUp:
+                                    Angle = (-1) * Angle;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            switch (position)
+                            {
+                                case FirstPointPosition.LeftUp:
+                                case FirstPointPosition.RightDown:
+                                    Angle = (-1) * Angle;
+                                    break;
+                            }
+                        }
+                        Graphics = Graphics.Rotate(Angle, background);
                     }
                 }
-                else
+                catch (Exception ex)
                 {
-                    switch (position)
-                    {
-                        case FirstPointPosition.LeftUp:
-                        case FirstPointPosition.RightDown:
-                            Angle = (-1) * Angle;
-                            break;
-                    }
+                    Info = ex.Message;
                 }
-                Graphics = Graphics.Rotate(Angle, background);
+                await Task.Delay(50);
+                IsBusy = false;
             }
         }
 
         string fileName;
-        private void SaveImage()
+        private async Task SaveImageAsync()
         {
-            var fileNameRotated = Path.GetFileNameWithoutExtension(fileName) + "_Rotated.png";
-            var path = Path.GetDirectoryName(fileName);
-            var fullPath = $"{path}\\{fileNameRotated}";
-            Graphics.Save(fullPath);
-            Info = $"{fileNameRotated} Saved Successfully";
-            signalForNextImage.Release();
-            PointA = new Point();
-            PointB = new Point();
-            Angle = 0;
+            try
+            {
+                var fileNameRotated = Path.GetFileNameWithoutExtension(fileName) + "_Rotated.png";
+                var path = Path.GetDirectoryName(fileName);
+                var fullPath = $"{path}\\{fileNameRotated}";
+                Graphics.Save(fullPath);
+                Info = $"{fileNameRotated} Saved Successfully";
+                await Task.Run(() => Graphics.Dispose());
+                signalForNextImage.Release();
+                PointA = new Point();
+                PointB = new Point();
+                Angle = 0;
+            }
+            catch (Exception ex)
+            {
+                Info = ex.Message;
+            }
         }
 
         private async void OpenImage()
@@ -166,8 +222,7 @@ namespace ImageRotator.Core
                 foreach (var file in openFile.FileNames)
                 {
                     fileName = file;
-                    GraphicsDefault = new Image<Bgr, byte>(fileName);
-                    Graphics = GraphicsDefault.Clone();
+                    Graphics = new Image<Bgr, byte>(fileName); ;
                     ImageCounterDisplay = $"Image Number {currentImageNumber}/{totalImages}";
                     await signalForNextImage.WaitAsync();
                     currentImageNumber++;
